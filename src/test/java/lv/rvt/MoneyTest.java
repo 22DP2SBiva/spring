8eeb26986440a18f002663eed3b14f22b10a25c8
22DP2SBiva <131154174+22DP2SBiva@users.noreply.github.com>
2024-01-30 06:52:24 +0000

    palabots

--- src/test/java/lv/rvt/MoneyTest.java
+++ src/test/java/lv/rvt/MoneyTest.java
@@ -3,6 +3,7 @@ import rvt.Money;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -15,7 +16,20 @@ public class MoneyTest {
         assertEquals(16, macins3.euros());
         assertEquals(0, macins3.cents());
     }
+    @Test
     public void testLessMethode(){
-        //assertFalse(macins1.lessThen(macins2));
+        Money macins1 = new Money(10, 10);
+        Money macins2 = new Money(5, 90);
+        assertFalse(macins1.lessThan(macins2));
+        assertTrue(macins2.lessThan(macins1));
+    } 
+    @Test
+    public void testMinusMethode(){
+        Money macins1 = new Money(5, 10);
+        Money macins2 = new Money(6, 0);
+        Money macins3 = macins1.minus(macins2);
+        Money macins4 = macins2.minus(macins1);
+        assertEquals(0, macins3.euros()); assertEquals(0, macins3.cents());
+        assertEquals(0, macins4.euros()); assertEquals(90, macins4.cents());
     }
 }
